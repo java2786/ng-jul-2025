73441496c9cf319ed18b514c3e99f0ecedf01f5d
Arun Kumar <java.kumar.arun@gmail.com>
2025-09-27 13:31:49 +0530

    27sep

---
+++ angular_tutorials/angular_tutorials/src/6_login_form/components/login-form/login-form.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-login-form',
+  // providers: [BrowserModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
+  templateUrl: './login-form.html',
+  styleUrl: './login-form.scss',
+})
+export class LoginForm {
+  userForm:FormGroup;
+
+  // DI
+  constructor(private fb:FormBuilder){
+    this.userForm = this.fb.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  onLogin(){
+    console.log(this.userForm.value)
+    console.log(this.userForm.controls["email"].valid)
+    console.log(this.userForm.controls["password"].valid)
+  }
+}
+
